881127a1a7faebc024ce437d626fc5ce696fd783
harold1970 <109260941+harold1970@users.noreply.github.com>
2024-01-01 14:40:29 -0800

    hello world
    hello world in morse code for the NeoPixle

---
+++ hello_world.go
@@ -0,0 +1,99 @@
+package main
+import (
+	"machine"
+	"time"
+	)
+
+
+func main() {
+	
+	led := machine.PB02
+	led.configure(machine.PinConfig{Mode: machine.PinOutput})
+	for {
+		Led.Low()
+		time.Sleep(time.Millisecond * 1000)
+		hello()
+		newWord()
+		world()
+ 	    } 
+
+	println("hello world")
+	
+}
+
+func dot() {
+	Led.Low()
+	time.Sleep(time.Millisecond * 100)
+	Led.High()
+	time.Sleep(time.Millisecond * 500)
+}
+func dash() {
+	Led.Low()
+	time.Sleep(time.Millisecond * 100)
+	Led.High()
+	time.Sleep(time.Millisecond * 1000)
+}
+
+func space() {
+	Led.Low()
+	time.Sleep(time.Millisecond * 1000)
+
+
+
+}
+
+func hello() {
+	dot()
+	dot()
+	dot()
+	dot()
+	space()
+	dot()
+	space()
+	dot()
+	dash()
+	dot()
+	dot()
+	space()
+	dot()
+	dash()
+	dot()
+	dot()
+	space()
+	dash()
+	dash()
+	dash()
+
+	}
+
+
+}
+func world(){
+
+	dot()
+	dash()
+	dash()
+	space()
+	dash()
+	dash()
+	dash()
+	space()
+	dot()
+	dash()
+	dot()
+	space()
+	dot()
+	dash()
+	dot()
+	dot()
+
+}
+
+func newWord() { 
+	
+	space()
+	space()
+
+
+
+}
